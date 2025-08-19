cb5da25570542c4b35a7079fb239fab086d40105
Andrey Bragin <andrey.bragin@jetbrains.com>
2025-08-19 10:52:01 +0200

    [examples] Add trip planning agent (#595)

---
+++ examples/trip-planning-example/settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "koog-trip-planning"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        // Public JetBrains repo with dev Koog builds
+        maven(url = "https://packages.jetbrains.team/maven/p/grazi/grazie-platform-public")
+    }
+}
