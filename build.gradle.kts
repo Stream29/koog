2e0d9fe1191daa7e8bccbc466b3bd4c1d6334349
kpavlov <1517853+kpavlov@users.noreply.github.com>
2025-08-28 14:56:37 +0300

    Update iOS simulator test workflow to include linking steps, update version to 0.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ group = "ai.koog"
 version = run {
     // our version follows the semver specification
 
-    val main = "0.4.0.1"
+    val main = "0.4.1"
 
     val feat = run {
         val releaseBuild = !System.getenv("BRANCH_KOOG_IS_RELEASING_FROM").isNullOrBlank()
